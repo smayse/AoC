3b37ba158720ad98ba4707d99f9ae9860483e6bb
Scott Mayson - Win10 <smayson@outlook.com>
2023-12-04 10:20:31 -0700

    FInish Day2

--- AdventOfCode/Day_01.cs
+++ AdventOfCode/Day_01.cs
@@ -16,17 +16,16 @@ namespace AdventOfCode
         {
             _input = File.ReadAllLines(InputFilePath);
         }
-        public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateSum1()}, part 1");
+        public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateSumPart1()}, part 1");
 
-        public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateSum2()}, part 2");
+        public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateSumPart2()}, part 2");
 
-        private string CalculateSum1()
+        private string CalculateSumPart1()
         {
             var sum = 0;
             foreach (var line in _input)
             {
-                var result = line.Replace(_writtenDigits[0], "one1one").Replace(_writtenDigits[1], "two2two").Replace(_writtenDigits[2], "three3three").Replace(_writtenDigits[3], "four4four").Replace(_writtenDigits[4], "five5five").Replace(_writtenDigits[5], "six6six").Replace(_writtenDigits[6], "seven7seven").Replace(_writtenDigits[7], "eight8eight").Replace(_writtenDigits[8], "nine9nine");
-                var numbers = new string(result.Where(char.IsDigit).ToArray());
+                var numbers = new string(line.Where(char.IsDigit).ToArray());
                 int.TryParse(numbers.First().ToString(), out var first);
                 int.TryParse(numbers.Last().ToString(), out var last);
                 int add = (first * 10) + last;
@@ -36,22 +35,16 @@ namespace AdventOfCode
             return sum.ToString();
         }
 
-        private string CalculateSum2()
+        private string CalculateSumPart2()
         {
             var sum = 0;
             foreach (var line in _input)
             {
                 var result = line.Replace(_writtenDigits[0], "one1one").Replace(_writtenDigits[1], "two2two").Replace(_writtenDigits[2], "three3three").Replace(_writtenDigits[3], "four4four").Replace(_writtenDigits[4], "five5five").Replace(_writtenDigits[5], "six6six").Replace(_writtenDigits[6], "seven7seven").Replace(_writtenDigits[7], "eight8eight").Replace(_writtenDigits[8], "nine9nine");
-                int firstDigit = 0;
-                int lastDigit = 0;
-                int number = Convert.ToInt32(Regex.Replace(result, @"\D", ""));
-                lastDigit = System.Math.Abs(number % 10);
-                while (number != 0)
-                {
-                    firstDigit = System.Math.Abs(number % 10);
-                    number /= 10;
-                }
-                int add = (firstDigit * 10) + lastDigit;
+                var numbers = new string(result.Where(char.IsDigit).ToArray());
+                int.TryParse(numbers.First().ToString(), out var first);
+                int.TryParse(numbers.Last().ToString(), out var last);
+                int add = (first * 10) + last;
                 sum += add;
             }
 
--- AdventOfCode/Day_02.cs
+++ AdventOfCode/Day_02.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Spectre.Console;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,45 +12,103 @@ namespace AdventOfCode
     public class Day_02 : BaseDay
     {
         private readonly string[] _input;
+        private readonly int maxRed = 12;
+        private readonly int maxGreen = 13;
+        private readonly int maxBlue = 14;
         public Day_02()
         {
             _input = File.ReadAllLines(InputFilePath);
         }
-        public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateSum1()}, part 1");
+        public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateSumPart1()}, part 1");
 
-        public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateSum2()}, part 2");
+        public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateSumPart2()}, part 2");
 
-        private string CalculateSum1()
+        private string CalculateSumPart1()
         {
             var sum = 0;
-            foreach (var line in _input)
+            for (var index = 0; index < _input.Length; index++)
             {
-                var numbers = new string(line.Where(char.IsDigit).ToArray());
-                int.TryParse(numbers.First().ToString(), out var first);
-                int.TryParse(numbers.Last().ToString(), out var last);
-                int add = (first * 10) + last;
-                sum += add;
+                var line = _input[index];
+                var game = line.Split(':');
+                var rounds = game[1].Split(';');
+                var possible = true;
+                foreach (var round in rounds)
+                {
+                    var countRed = 0;
+                    var countGreen = 0;
+                    var countBlue = 0;
+                    var colors = round.Split(',');
+                    foreach (var color in colors)
+                    {
+                        var countString = Regex.Match(color, @"\d+").Value;
+                        if (color.Contains("red"))
+                        {
+                            countRed += int.Parse(countString);
+                            if (countRed <= maxRed) continue;
+                            possible = false;
+                            break;
+                        }
+                        else if (color.Contains("green"))
+                        {
+                            countGreen += int.Parse(countString);
+                            if (countGreen <= maxGreen) continue;
+                            possible = false;
+                            break;
+                        }
+                        else if (color.Contains("blue"))
+                        {
+                            countBlue += int.Parse(countString);
+                            if (countBlue <= maxBlue) continue;
+                            possible = false;
+                            break;
+                        }
+                    }
+                    if (!possible) break;
+                }
+                if (possible) sum += index + 1;
             }
 
             return sum.ToString();
         }
 
-        private string CalculateSum2()
+        private string CalculateSumPart2()
         {
             var sum = 0;
             foreach (var line in _input)
             {
-                int firstDigit = 0;
-                int lastDigit = 0;
-                int number = Convert.ToInt32(Regex.Replace(line, @"\D", ""));
-                lastDigit = System.Math.Abs(number % 10);
-                while (number != 0)
+                var game = line.Split(':');
+                var rounds = game[1].Split(';');
+                var needRed = 0;
+                var needGreen = 0;
+                var needBlue = 0;
+                foreach (var round in rounds)
                 {
-                    firstDigit = System.Math.Abs(number % 10);
-                    number /= 10;
+                    var colors = round.Split(',');
+                    foreach (var color in colors)
+                    {
+                        var countString = Regex.Match(color, @"\d+").Value;
+                        if (color.Contains("red"))
+                        {
+                            var countRed = int.Parse(countString);
+                            if (countRed <= needRed) continue;
+                            needRed = countRed;
+                        }
+                        else if (color.Contains("green"))
+                        {
+                            var countGreen = int.Parse(countString);
+                            if (countGreen <= needGreen) continue;
+                            needGreen = countGreen;
+                        }
+                        else if (color.Contains("blue"))
+                        {
+                            var countBlue = int.Parse(countString);
+                            if (countBlue <= needBlue) continue;
+                            needBlue = countBlue;
+                        }
+                    }
                 }
-                int add = (firstDigit * 10) + lastDigit;
-                sum += add;
+                var power = needRed * needGreen * needBlue;
+                sum += power;
             }
 
             return sum.ToString();
